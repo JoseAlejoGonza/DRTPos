11e38dae0b2b1a3162402729fddee89ebcf7838e
JoseAlejoGonza <jose_alejogonza@hotmail.com>
2025-10-01 19:28:43 -0500

    add shop component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CategoryComponent } from './components/category-component/category.component';
 import { ProductsComponent } from './components/products/products.component';
 import { HomeComponent } from './components/home/home.component';
+import { ShopComponent } from './components/shop/shop.component';
 
 
 export const routes: Routes = [
 	{ path: 'home', component: HomeComponent },
 	{ path: 'products', component: ProductsComponent },
 	{ path: 'categories', component: CategoryComponent },
+	{ path: 'shop', component: ShopComponent }
 ];
